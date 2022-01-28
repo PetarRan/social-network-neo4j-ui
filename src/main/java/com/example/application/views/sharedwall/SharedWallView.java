251ff837c1bc03aae11c5af20484db1e886d6080
Petar Randjelovic <petarran@elfak.rs>
2022-01-28 13:41:58 +0100

    -

---
+++ src/main/java/com/example/application/views/sharedwall/SharedWallView.java
@@ -0,0 +1,43 @@
+package com.example.application.views.sharedwall;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.HasStyle;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.littemplate.LitTemplate;
+import com.vaadin.flow.component.select.Select;
+import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Shared Wall")
+@Route(value = "wall", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+@Tag("shared-wall-view")
+@JsModule("./views/sharedwall/shared-wall-view.ts")
+public class SharedWallView extends LitTemplate implements HasComponents, HasStyle {
+
+    @Id
+    private Select<String> sortBy;
+
+    public SharedWallView() {
+        addClassNames("shared-wall-view", "flex", "flex-col", "h-full");
+        sortBy.setItems("Popularity", "Newest first", "Oldest first");
+        sortBy.setValue("Popularity");
+
+        add(new SharedWallViewCard("Snow mountains under stars",
+                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
+        add(new SharedWallViewCard("Snow covered mountain",
+                "https://images.unsplash.com/photo-1512273222628-4daea6e55abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
+        add(new SharedWallViewCard("River between mountains",
+                "https://images.unsplash.com/photo-1536048810607-3dc7f86981cb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"));
+        add(new SharedWallViewCard("Milky way on mountains",
+                "https://images.unsplash.com/photo-1515705576963-95cad62945b6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"));
+        add(new SharedWallViewCard("Mountain with fog",
+                "https://images.unsplash.com/photo-1513147122760-ad1d5bf68cdb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"));
+        add(new SharedWallViewCard("Mountain at night",
+                "https://images.unsplash.com/photo-1562832135-14a35d25edef?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=815&q=80"));
+    }
+}
